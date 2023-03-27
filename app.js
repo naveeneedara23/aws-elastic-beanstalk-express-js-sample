b6d222230ea2cb1661466869b98c31500440d69a
naveeneedara23 <enaveendevops@gmail.com>
2023-03-27 17:32:41 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Have a fresh start!'));
+app.get('/', (req, res) => res.send('Have a fresh start code,build,review,deploy!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
